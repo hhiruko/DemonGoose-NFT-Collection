ae1e0fc397b6e8d93c4a2516ecec0de9b24e799e
menotbf <menotyourbestfriend@gmail.com>
2022-09-22 19:17:19 +0600

    url change

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,4 +1,4 @@
 // URL to extract Metadata for a Crypto Dev NFT replace after some time
-const METADATA_URL = "https://demon-goose-nft-collection-my-app-hjdq-git-master-hhiruko.vercel.app//api/";
+const METADATA_URL = "https://demon-goose-nft-collection-my-app-hjdq-git-master-hhiruko.vercel.app/api/";
 
 module.exports = { METADATA_URL };
